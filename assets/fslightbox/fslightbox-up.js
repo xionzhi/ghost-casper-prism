ac68e913d12978e4bd7cf0dc7a09efe54482efc3
xionzhi <xizh97@gmail.com>
2022-04-13 13:33:02 +0800

    fix: load fslightbox

--- assets/fslightbox/fslightbox-up.js
+++ assets/fslightbox/fslightbox-up.js
@@ -1,14 +1,28 @@
-const images = document.querySelectorAll('.post-content img');
+var codeElement = document.querySelector(".post-content img");
 
-// Lightbox function
-images.forEach(function (image) {
-  var wrapper = document.createElement('a');
-  wrapper.setAttribute('data-no-swup', '');
-  wrapper.setAttribute('data-fslightbox', '');
-  wrapper.setAttribute('href', image.src);
-  wrapper.setAttribute('aria-label', 'Click for Lightbox');
-  image.parentNode.insertBefore(wrapper, image.parentNode.firstChild);
-  wrapper.appendChild(image);
-});
+if (typeof(codeElement) != 'undefined' && codeElement != null)
+{
+  // load 
+  var fslightboxScript = document.createElement('script');
+  fslightboxScript.type='text/javascript';
+  fslightboxScript.src="/assets/fslightbox/fslightbox.js?v=1";
+  fslightboxScript.onload = function() {
+    
+  };
+  document.head.appendChild(fslightboxScript);
 
-refreshFsLightbox();
+  const images = document.querySelectorAll('.post-content img');
+
+  // Lightbox function
+  images.forEach(function (image) {
+    var wrapper = document.createElement('a');
+    wrapper.setAttribute('data-no-swup', '');
+    wrapper.setAttribute('data-fslightbox', '');
+    wrapper.setAttribute('href', image.src);
+    wrapper.setAttribute('aria-label', 'Click for Lightbox');
+    image.parentNode.insertBefore(wrapper, image.parentNode.firstChild);
+    wrapper.appendChild(image);
+  });
+
+  refreshFsLightbox();
+}
